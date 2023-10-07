f059d683145e74a3059c98f5a552c4670f1a66eb
Mr.Black <westin007@gmail.com>
2023-10-06 22:29:26 -0500

    added cubari converter - Mr. Black

---
+++ src/scripts/cubari.js
@@ -0,0 +1,28 @@
+const GalleryIdTest = /^(\w){7}$/i
+const validLinkTest = /^(https:\/\/)?imgur\.com\/(a|gallery)\/.......$/i
+const idPart = /(a|gallery)\/.......$/
+
+function isFullLink(input){
+    const testString = input.trim()
+    return validLinkTest.test(testString)
+}
+
+function isGalleryId(input){
+    const testString = input.trim()
+    return GalleryIdTest.test(testString)
+}
+
+function isValidLink(input){
+    //https://imgur.com/a/ruOqLxv
+
+    return isFullLink(input) || isGalleryId(input)
+}
+
+function extractIdFromLink(input){
+    if(!isFullLink(input)) return ""
+    var matches = idPart.matches(input.trim())
+    if(matches === null) return ""
+    return matches[0].split("/")[2]
+}
+
+export {isValidLink, isGalleryId, extractIdFromLink}
