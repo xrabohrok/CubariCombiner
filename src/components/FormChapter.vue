16ddab06ec416bcbbb159c973e243b76c9b605de
Mr.Black <westin007@gmail.com>
2023-11-11 18:50:02 -0600

    multi group chapters - Mr.Black

--- src/components/FormChapter.vue
+++ src/components/FormChapter.vue
@@ -13,12 +13,16 @@ const model = ref({
     timestamp: new Date(),
 })
 
-const groupSets = ref([
-    {
+function blankGroup(){
+    return {
         id: uuidv4(),
         groupName: "",
         imgur: "",
     }
+}
+
+const groupSets = ref([
+    blankGroup(),
 ])
 
 function simplifyModel(model, groups) {
@@ -28,6 +32,24 @@ function simplifyModel(model, groups) {
         timestamp: model.timestamp,
         refId: props.refId
     }
+}
+
+function addGroup(){
+    groupSets.value.push(
+        blankGroup()
+    )
+}
+
+function deleteGroup(id){
+    var targetIndex = groupSets.value.findIndex(group => {
+      return group.id === id
+    })
+    if (targetIndex < 0) return
+    groupSets.value.splice(targetIndex,1)
+}
+
+function allowGroupDelete(groupSet){
+    return groupSet.length >1
 }
 
 defineEmits({
@@ -42,20 +64,30 @@ defineEmits({
 </script>
 <template>
     <div class="chapter-row">
-        <div class="chapter-label">{{ props.chapterNum }}</div>
+        <div class="chapter-label">
+            <button name="Remove" @click="$emit('chapterRemove', props.refId)" :disabled="props.total<=1">X</button> 
+            {{ props.chapterNum }}
+        </div>
         <div class="form-chapter">
-            <label>Chapter Title</label><input type="text" v-model="model.chapterTitle"
+            <label>Chapter Title</label>
+            <input type="text" v-model="model.chapterTitle"
                     @change="$emit('formChanged', simplifyModel(model, groupSets))" />
-            <div class="groups" v-for="g in groupSets" :key="g.id">
-                <label>Group Name</label><input type="text" v-model="g.groupName"
+            <label class="timestamp">Timestamp</label>
+            <VueDatePicker v-model="model.timestamp" :disabled="props.lockedTime" 
+                @update:model-value="$emit('formChanged', simplifyModel(model, groupSets))" auto-apply />
+            <div class="groups" v-for="(g, i) in groupSets" :key="g.id">
+                <div class="group-label">
+                    <button name="GroupRemove" @click="deleteGroup(g.id)" :disabled="!allowGroupDelete(groupSets)">X</button> 
+                    {{ i+1 }}
+                </div>
+                <label> Group Name</label><input type="text" v-model="g.groupName"
                         @change="$emit('formChanged', simplifyModel(model, groupSets))" />
                 <label>Imgur Link</label><input type="text" v-model="g.imgur"
                         @change="$emit('formChanged', simplifyModel(model, groupSets))" />
             </div>
+            <button name="GroupAdd" class="group-add-button" @click="addGroup()">Add Group</button>
             <div class="imgur-preview"></div>
-            <label class="timestamp">Timestamp</label><VueDatePicker v-model="model.timestamp" :disabled="props.lockedTime" 
-                @update:model-value="$emit('formChanged', simplifyModel(model, groupSets))" auto-apply />
+            
         </div>
-        <button name="Remove" @click="$emit('chapterRemove', props.refId)" :disabled="props.total<=1">X</button>
     </div>
 </template>
